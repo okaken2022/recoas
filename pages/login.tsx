04ed89b3cf29efbd3288b8337e7267f9a4f8dfd9
okaken2022 <okaken.address@gmail.com>
2023-04-11 10:24:51 +0900

    ログイン機能

---
+++ pages/login.tsx
@@ -0,0 +1,67 @@
+import { useAuth, useUser } from '@/hooks/firebase';
+import { Button, FormLabel, Input, VStack, Box, Text } from '@chakra-ui/react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type Inputs = {
+  email: string;
+  password: string;
+  confirmationPassword: string;
+};
+
+export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const auth = useAuth();
+  const currentUser = useUser();
+  const router = useRouter();
+
+  //login機能
+  const login = async (email: string, password: string) => {
+    try {
+      const UserCredential = await signInWithEmailAndPassword(auth, email, password);
+      console.log('ログイン成功');
+      router.push('/');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const onSubmit: SubmitHandler<Inputs> = ({ email, password }) => {
+    login(email, password);
+  };
+
+  useEffect(() => {
+    if (currentUser) router.push('/');
+  }, [currentUser, router]);
+
+  return (
+    <>
+      <Box bg='#3778B8' w='100%' p={4} color='white' mb={100}>
+        <Text fontSize='2xl'>
+          Todo List
+        </Text>
+      </Box>
+      <VStack w='30vw' mx='auto'>
+        <FormLabel htmlFor='name'>メールアドレス</FormLabel>
+        <Input id='name' placeholder='name' {...register('email', { required: true })} />
+        <FormLabel htmlFor='password'>パスワード</FormLabel>
+        <Input
+          id='password'
+          placeholder='password'
+          type='password'
+          {...register('password', { required: true })}
+        />
+        <Button mt={4} colorScheme='teal' onClick={handleSubmit(onSubmit)}>
+          ログイン
+        </Button>
+      </VStack>
+    </>
+  );
+}
