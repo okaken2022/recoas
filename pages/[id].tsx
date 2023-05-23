25c2ffb1a8db410afb25aa2890572b614caf5fc7
okaken2022 <okaken.address@gmail.com>
2023-05-23 11:14:34 +0900

    編集ページ　ヘッダー追加

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -2,19 +2,21 @@ import { Box, Button, ButtonGroup, Heading, Spacer, Text } from '@chakra-ui/reac
 import { DocumentData } from 'firebase/firestore';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { Header } from '@/components/Header';
 
 export default function detail(todo: DocumentData) {
   return (
     <>
-      <Text>編集ページです</Text>
-      <Box p='2'>
-        <Heading size='md'>{todo.title}</Heading>
-      </Box>
-      <Spacer />
-      <ButtonGroup gap='2'>
-        <Button colorScheme='red'>削除</Button>
-      </ButtonGroup>
-      <Link href='/'>一覧へ</Link>
+    <Header />
+    <Box p='4'>
+    <Text>編集ページです</Text>
+      <Heading size='md'>{todo.title}</Heading>
+    <Spacer />
+    <ButtonGroup gap='2'>
+      <Button colorScheme='red'>削除</Button>
+    </ButtonGroup>
+    <Link href='/'>一覧へ</Link>
+    </Box>
     </>
   );
 }
