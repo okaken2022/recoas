6ab7606e3be896a7f5b122f9309e88d5eaf7c258
okaken2022 <okaken.address@gmail.com>
2023-06-05 08:11:16 +0900

    fix firebase.tsx

--- hooks/firebase.tsx
+++ hooks/firebase.tsx
@@ -7,12 +7,12 @@ import { NextRouter } from 'next/router';
 import { User } from '../types/user';
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_APIKEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECTID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APPID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
