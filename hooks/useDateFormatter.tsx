0426aeed76c9f6bae16fd01d6f845bf7c7219cda
okaken2022 <okaken.address@gmail.com>
2023-08-27 10:04:13 +0900

    カスタムフック作成 useDateFormatter

---
+++ hooks/useDateFormatter.tsx
@@ -0,0 +1,16 @@
+import dayjs from 'dayjs';
+
+export function useDateFormatter(date: string) {
+  const formattedMonth = dayjs(date).format('YYYY-MM');
+  const formattedMonthJa = dayjs(date).format('YYYY年MM月');
+  const formattedDate = dayjs(date).format('YYYY-MM-DD');
+  const weekdayNamesJP = ['日', '月', '火', '水', '木', '金', '土'];
+  const formattedDateJa = dayjs(date).format(`YYYY年M月D日 (${weekdayNamesJP[dayjs(date).day()]})`);
+
+  return {
+    formattedMonth,
+    formattedMonthJa,
+    formattedDate,
+    formattedDateJa
+  };
+}
