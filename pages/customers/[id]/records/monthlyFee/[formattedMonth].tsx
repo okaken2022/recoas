ce4506edbf514cebe8fa17f03e52eb6b82467f2b
okaken2022 <okaken.address@gmail.com>
2024-08-26 17:24:36 +0900

    style: 工賃表修正

--- pages/customers/[id]/records/monthlyFee/[formattedMonth].tsx
+++ pages/customers/[id]/records/monthlyFee/[formattedMonth].tsx
@@ -1,17 +1,7 @@
-import {
-  Text,
-  Heading,
-  Spinner,
-  Flex,
-  Grid,
-  GridItem,
-  Center,
-  Box,
-  Spacer,
-} from '@chakra-ui/react';
+import { Text, Heading, Spinner, Flex, Grid, GridItem, Center, Spacer } from '@chakra-ui/react';
 import { useAuth, db, AuthContext } from '@/hooks/firebase';
-import { NextRouter, useRouter } from 'next/router';
-import { collection, getDocs } from 'firebase/firestore';
+import { type NextRouter, useRouter } from 'next/router';
+import { collection, getDocs, Timestamp } from 'firebase/firestore';
 
 import Layout from '@/components/Layout';
 import { useContext, useEffect, useState } from 'react';
@@ -20,48 +10,37 @@ import dayjs from 'dayjs';
 import 'dayjs/locale/ja';
 dayjs.locale('ja');
 
-import { CustomerInfoType } from '@/types/customerInfo';
+import type { CustomerInfoType } from '@/types/customerInfo';
 import { fetchCustomer } from '@/utils/fetchCustomer';
 import { useDateFormatter } from '@/hooks/useDateFormatter';
 
 export default function RecordMonthPage() {
-  {
-    /* ログイン */
-  }
   const auth = useAuth();
   const currentUser = auth.currentUser;
   const user = useContext(AuthContext);
   const router: NextRouter = useRouter();
 
-  {
-    /* 利用者情報取得 */
-  }
+  // 利用者情報取得
   const { id: customerId, formattedMonth } = router.query;
   const [customer, setCustomer] = useState<CustomerInfoType | null>(null);
 
-  {
-    /* 日付情報 */
-  }
+  // 日付情報
+
   const { date } = router.query as { date: string };
   const { formattedMonthJa } = useDateFormatter(date);
 
-  {
-    /* 月別工賃表リスト取得 */
-  }
+  // 月別工賃表リスト取得
+
   type DailyRecordData = {
     id: string;
     author: string;
     amWork: string;
     pmWork: string;
     timeAdjustment: {
-      amStartTimeHours: number;
-      amStartTimeMinutes: number;
-      amFinishTimeHours: number;
-      amFinishTimeMinutes: number;
-      pmStartTimeHours: number;
-      pmStartTimeMinutes: number;
-      pmFinishTimeHours: number;
-      pmFinishTimeMinutes: number;
+      amStartTime: Timestamp;
+      amFinishTime: Timestamp;
+      pmStartTime: Timestamp;
+      pmFinishTime: Timestamp;
     };
   };
 
@@ -70,8 +49,6 @@ export default function RecordMonthPage() {
   const fetchData = async () => {
     if (Array.isArray(customerId) || Array.isArray(formattedMonth)) return;
     try {
-      // dailyRecordsコレクション内のドキュメントを取得
-      // as string はできるだけ使わない。関数で型ガードするのがベター？if文使うか、
       const dailyRecordsCollectionRef = collection(
         db,
         'customers',
@@ -82,30 +59,36 @@ export default function RecordMonthPage() {
       );
       const dailyRecordsQuerySnapshot = await getDocs(dailyRecordsCollectionRef);
 
-      // dailyRecordsコレクション内の各ドキュメントに対して、singleRecordコレクション内のドキュメントを取得
       const dailyRecordPromises = dailyRecordsQuerySnapshot.docs.map(async (doc) => {
         const dailyRecordData = doc.data();
         const dailyRecordBasicData = {
           id: doc.id,
-          author: dailyRecordData.author,
-          amWork: dailyRecordData.amWork,
-          pmWork: dailyRecordData.pmWork,
+          author: dailyRecordData.author || '---',
+          amWork: dailyRecordData.amWork || '---',
+          pmWork: dailyRecordData.pmWork || '---',
           timeAdjustment: {
-            amStartTimeHours: dailyRecordData.timeAdjustment.amStartTimeHours,
-            amStartTimeMinutes: dailyRecordData.timeAdjustment.amStartTimeMinutes,
-            amFinishTimeHours: dailyRecordData.timeAdjustment.amFinishTimeHours,
-            amFinishTimeMinutes: dailyRecordData.timeAdjustment.amFinishTimeMinutes,
-            pmStartTimeHours: dailyRecordData.timeAdjustment.pmStartTimeHours,
-            pmStartTimeMinutes: dailyRecordData.timeAdjustment.pmStartTimeMinutes,
-            pmFinishTimeHours: dailyRecordData.timeAdjustment.pmFinishTimeHours,
-            pmFinishTimeMinutes: dailyRecordData.timeAdjustment.pmFinishTimeMinutes,
+            amStartTime:
+              dailyRecordData.timeAdjustment?.amStartTime
+                ?.toDate()
+                .toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit' }) || '---',
+            amFinishTime:
+              dailyRecordData.timeAdjustment?.amFinishTime
+                ?.toDate()
+                .toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit' }) || '---',
+            pmStartTime:
+              dailyRecordData.timeAdjustment?.pmStartTime
+                ?.toDate()
+                .toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit' }) || '---',
+            pmFinishTime:
+              dailyRecordData.timeAdjustment?.pmFinishTime
+                ?.toDate()
+                .toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit' }) || '---',
           },
         };
 
         return dailyRecordBasicData;
       });
 
-      // データをセット
       const recordData = await Promise.all(dailyRecordPromises);
       setAllRecordData(recordData);
     } catch (error) {
@@ -113,12 +96,7 @@ export default function RecordMonthPage() {
     }
   };
 
-  // dailyRecord.timeAdjustment.amStartTimeMinutesを0埋めして表示する関数
-  const formatMinutes = (minutes: number) => {
-    // 2桁の文字列に変換し、0で埋める
-    return String(minutes).padStart(2, '0');
-  };
-
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
     if (customerId) {
       const id = Array.isArray(customerId) ? customerId[0] : customerId;
@@ -154,74 +132,94 @@ export default function RecordMonthPage() {
         {/* 工賃表 */}
         <Grid
           // align="center"
-          border='1px'
           h='50px'
           templateRows='repeat(2, 1fr)'
           templateColumns='repeat(10, 1fr)'
+          fontSize={{ base: '12px', md: '16px' }}
         >
-          <GridItem rowSpan={2} colSpan={2} bg='color.mainTransparent1' border='1px'>
+          <GridItem
+            rowSpan={2}
+            colSpan={2}
+            bg='color.mainTransparent1'
+            p='2px'
+            borderTopLeftRadius='8px'
+          >
             日付
           </GridItem>
-          <GridItem rowSpan={1} colSpan={4} bg='color.mainTransparent2' border='1px'>
+          <GridItem rowSpan={1} colSpan={4} bg='color.mainTransparent2' p='2px'>
             午前
           </GridItem>
-          <GridItem rowSpan={1} colSpan={4} bg='color.mainTransparent2' border='1px'>
+          <GridItem
+            rowSpan={1}
+            colSpan={4}
+            bg='color.mainTransparent2'
+            p='2px'
+            borderTopRightRadius='8px'
+          >
             午後
           </GridItem>
-          <GridItem rowSpan={1} colSpan={1} borderRight='1px' borderLeft='1px'>
+          <GridItem rowSpan={1} colSpan={1} p='2px'>
             開始
           </GridItem>
-          <GridItem rowSpan={1} colSpan={1} borderRight='1px'>
+          <GridItem rowSpan={1} colSpan={1} p='2px'>
             終了
           </GridItem>
-          <GridItem rowSpan={1} colSpan={2} borderRight='1px'>
+          <GridItem rowSpan={1} colSpan={2} backgroundColor='gray.100' p='2px'>
             作業内容
           </GridItem>
-          <GridItem rowSpan={1} colSpan={1} borderRight='1px' borderLeft='1px'>
+          <GridItem rowSpan={1} colSpan={1} p='2px'>
             開始
           </GridItem>
-          <GridItem rowSpan={1} colSpan={1} borderRight='1px'>
+          <GridItem rowSpan={1} colSpan={1} p='2px'>
             終了
           </GridItem>
-          <GridItem rowSpan={1} colSpan={2} borderRight='1px'>
+          <GridItem rowSpan={1} colSpan={2} backgroundColor='gray.100' p='2px'>
             作業内容
           </GridItem>
         </Grid>
         {allRecordData.map((dailyRecord, index) => (
           <Grid
             key='index'
-            border='1px'
             borderColor='#333'
             templateRows='1fr'
             templateColumns='repeat(10, 1fr)'
+            fontSize={{ base: '12px', md: '16px' }}
           >
-            <GridItem colSpan={2} bg='color.mainTransparent1'>
+            <GridItem colSpan={2} bg='color.mainTransparent1' p='2px'>
               {dayjs(dailyRecord.id).format('M月D日(ddd)')}
             </GridItem>
-            <GridItem rowSpan={1} colSpan={1} borderRight='1px' borderLeft='1px'>
-              {dailyRecord.timeAdjustment.amStartTimeHours}:
-              {dailyRecord.timeAdjustment.amStartTimeMinutes}
-              {/* {formatMinutes(dailyRecord.timeAdjustment.amStartTimeMinutes)} */}
+            <GridItem rowSpan={1} colSpan={1} p='2px'>
+              {dailyRecord.timeAdjustment.amStartTime instanceof Timestamp
+                ? dailyRecord.timeAdjustment.amStartTime
+                    .toDate()
+                    .toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit' })
+                : dailyRecord.timeAdjustment.amStartTime}
             </GridItem>
-            <GridItem rowSpan={1} colSpan={1} borderRight='1px'>
-              {dailyRecord.timeAdjustment.amFinishTimeHours}:
-              {dailyRecord.timeAdjustment.amFinishTimeMinutes}
-              {/* {formatMinutes(dailyRecord.timeAdjustment.amFinishTimeMinutes)} */}
+            <GridItem rowSpan={1} colSpan={1} p='2px'>
+              {dailyRecord.timeAdjustment.amFinishTime instanceof Timestamp
+                ? dailyRecord.timeAdjustment.amFinishTime
+                    .toDate()
+                    .toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit' })
+                : dailyRecord.timeAdjustment.amFinishTime}
             </GridItem>
-            <GridItem rowSpan={1} colSpan={2} borderRight='1px'>
+            <GridItem rowSpan={1} colSpan={2} backgroundColor='gray.100' p='2px'>
               {dailyRecord.amWork}
             </GridItem>
-            <GridItem rowSpan={1} colSpan={1} borderRight='1px' borderLeft='1px'>
-              {dailyRecord.timeAdjustment.pmStartTimeHours}:
-              {dailyRecord.timeAdjustment.pmStartTimeMinutes}
-              {/* {formatMinutes(dailyRecord.timeAdjustment.pmStartTimeMinutes)} */}
+            <GridItem rowSpan={1} colSpan={1} p='2px'>
+              {dailyRecord.timeAdjustment.pmStartTime instanceof Timestamp
+                ? dailyRecord.timeAdjustment.pmStartTime
+                    .toDate()
+                    .toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit' })
+                : dailyRecord.timeAdjustment.pmStartTime}
             </GridItem>
-            <GridItem rowSpan={1} colSpan={1} borderRight='1px'>
-              {dailyRecord.timeAdjustment.pmFinishTimeHours}:
-              {dailyRecord.timeAdjustment.pmFinishTimeMinutes}
-              {/* {formatMinutes(dailyRecord.timeAdjustment.pmFinishTimeMinutes)} */}
+            <GridItem rowSpan={1} colSpan={1} p='2px'>
+              {dailyRecord.timeAdjustment.pmFinishTime instanceof Timestamp
+                ? dailyRecord.timeAdjustment.pmFinishTime
+                    .toDate()
+                    .toLocaleTimeString('ja-JP', { hour: '2-digit', minute: '2-digit' })
+                : dailyRecord.timeAdjustment.pmFinishTime}
             </GridItem>
-            <GridItem rowSpan={1} colSpan={2} borderRight='1px'>
+            <GridItem rowSpan={1} colSpan={2} backgroundColor='gray.100' p='2px'>
               {dailyRecord.pmWork}
             </GridItem>
           </Grid>
